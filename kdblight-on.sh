4cc15ebc1a7ac82d2e9974268f82e9541261d4f3
Danny Faerch Boensoee <mail@dannyfb.dk>
2018-04-03 18:26:59 +0200

    added 2nd light level

--- kdblight-on.sh
+++ kdblight-on.sh
@@ -3,7 +3,9 @@
 while read line; do
     if [[ $line == "2" ]]; then
         exit 0
+    elif [[ $line == "0" ]]; then
+        echo 1 | tee /sys/class/leds/tpacpi\:\:kbd_backlight/brightness 1> /dev/null
     else
         echo 2 | tee /sys/class/leds/tpacpi\:\:kbd_backlight/brightness 1> /dev/null
-    fi
+fi
 done < /sys/class/leds/tpacpi\:\:kbd_backlight/brightness
